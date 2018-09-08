517f2a22e6edb2780cc6fc6272a94d31c9ebb3b2
DESKTOP-UAQ1PI3\Quoc Tran <tranvanquocvq@gmail.com>
2018-09-08 11:07:06 +0700

    add web Api

--- ViettinShop.Data/ViettinShopDbContext.cs
+++ ViettinShop.Data/ViettinShopDbContext.cs
@@ -36,6 +36,8 @@ namespace ViettinShop.Data
 
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
 
+        public DbSet<Error> Errors { set; get; }
+
 
         protected override void OnModelCreating(DbModelBuilder builder)
         {
